2c07fbbc2fe61f456c10485df718874ed1f9b9a6
ADarkHero <maik.riedlsperger@gmail.com>
2019-07-25 08:42:18 +0200

    Merge and dark-mode.

--- inc/template_end.php
+++ inc/template_end.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap core JavaScript -->
     <script src="js/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
     
     <!-- Colorpicker -->
     <script type="text/javascript" src="js/jquery.minicolors.min.js"></script>
