1cfd462e53d4f37accc67b4ab6d50517ecd85e74
ADarkHero <maik.riedlsperger@gmail.com>
2019-11-27 13:03:16 +0100

    Added money change per month.

--- inc/yearly/chartMaker.php
+++ inc/yearly/chartMaker.php
@@ -1,19 +1,3 @@
-<script src="js/chartjs-plugin-datalabels.min.js"></script>
-
-
-<script>
-    function barChartOpenStatements(event, data) {
-        if (data[0]) {
-            var ticks = data[0]["_xScale"]["ticks"];
-            var index = data[0]["_index"];
-            var clickedValue = ticks[index];
-            var link = 'statement.php?search=' + clickedValue;
-            window.open(link, '_blank');
-        }
-    }
-</script>
-
-
 <?php
 $disablenext = false;
 if (isset($_GET["year"])) {
@@ -25,44 +9,43 @@ if (isset($_GET["year"])) {
 if ($year == date("Y")) {
     $disablenext = true;
 }
-
-$yearStart = $year . "-01-01";
-$yearEnd = $year . "-12-31";
-
-$sql = "SELECT Value, EntryDate, CategoryName, CategoryColor FROM statements "
-        . "LEFT JOIN categories ON statements.CategoryID = categories.CategoryID "
-        . "WHERE EntryDate >= '" . $yearStart . "' AND EntryDate <= '" . $yearEnd . "' "
-        . "AND CategoryExcludeStats = 0 ORDER BY EntryDate";
-$result = $conn->query($sql);
-
-//$moneySpent = 0; //Total money spent
-$moneyByDate = array(); //Money by date
-$moneyCategories = array(); //Money spent by category
-$categorieColors = array(); //Colors of the categories
-if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        //$moneySpent += doubleval(str_replace(',', '.', $row["Value"]));
-
-        if (!isset($moneyCategories[$row["CategoryName"]])) { //If the category has no value yet, set it.
-            $moneyCategories[$row["CategoryName"]] = 0;
-        }
-
-        if (!isset($categorieColors[$row["CategoryName"]])) { //Do we already have the category color?
-            $categorieColors[$row["CategoryName"]] = $row["CategoryColor"];
-        }
-        $moneyCategories[$row["CategoryName"]] += doubleval(str_replace(',', '.', $row["Value"]));
-
-        if (!isset($moneyByDate[$row["EntryDate"]])) { //If the date has no value yet, set it.
-            $moneyByDate[$row["EntryDate"]] = 0;
-        }
-        $moneyByDate[$row["EntryDate"]] += doubleval(str_replace(',', '.', $row["Value"]));
-    }
-} else {
-    //echo "You didn't spend money since your last paycheck. ";
-}
-arsort($moneyCategories);
 ?>
+
+
+<script src="js/chartjs-plugin-datalabels.min.js"></script>
+
+<script>
+    function chartOpenStatements(event, data) {
+        if (data[0]) {
+            var ticks = data[0]["_xScale"]["ticks"];
+            var index = data[0]["_index"];
+            var clickedValue = ticks[index];
+
+            try {
+                //All months
+                var months = ["January", "February", "March", "April",
+                    "May", "June", "July", "August",
+                    "September", "October", "November", "December"];
+                //Get number of the month
+                var monthNumber = months.indexOf(clickedValue) + 1;
+                //If the month is smaller than 10, add a 0 in front of it.
+                if (monthNumber < 10) {
+                    clickedValue = "-0" + monthNumber + "-";
+                } else {
+                    clickedValue = "-" + monthNumber + "-";
+                }
+                //Add year
+                clickedValue = <?php echo $year; ?> + clickedValue;
+            } catch (e) {
+
+            }
+
+            var link = 'statement.php?search=' + clickedValue;
+            window.open(link, '_blank');
+        }
+    }
+</script>
+
 
 <div class="row">
     <div class="col-12 col-md-9"><h1 class="mt-3">Yearly statistics</h1></div>
@@ -87,9 +70,57 @@ arsort($moneyCategories);
 
 <div class="containter">
     <div class="row">
-        <div class="col-12 col-lg-12"><h3 class="mt-5">Money spent this year per categories</h3><canvas id="moneySpent"></canvas></div> 
+        <div class="col-12 col-lg-12"><h3 class="mt-5">Money spent per year per categories</h3><canvas id="moneySpent"></canvas></div> 
+    </div>
+    <div class="row">
+        <div class="col-12 col-lg-12"><h3 class="mt-5">Money change per month</h3><canvas id="moneyTime"></canvas></div> 
     </div>
 </div>    
+
+
+<?php
+$yearStart = $year . "-01-01";
+$yearEnd = $year . "-12-31";
+
+$sql = "SELECT Value, EntryDate, CategoryName, CategoryColor FROM statements "
+        . "LEFT JOIN categories ON statements.CategoryID = categories.CategoryID "
+        . "WHERE EntryDate >= '" . $yearStart . "' AND EntryDate <= '" . $yearEnd . "' "
+        . "AND CategoryExcludeStats = 0 ORDER BY EntryDate";
+$result = $conn->query($sql);
+
+$moneyByDate = array(); //Money by date
+$moneyByMonth = array(); //Money by month
+$moneyCategories = array(); //Money spent by category
+$categorieColors = array(); //Colors of the categories
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        if (!isset($moneyCategories[$row["CategoryName"]])) { //If the category has no value yet, set it.
+            $moneyCategories[$row["CategoryName"]] = 0;
+        }
+
+        if (!isset($categorieColors[$row["CategoryName"]])) { //Do we already have the category color?
+            $categorieColors[$row["CategoryName"]] = $row["CategoryColor"];
+        }
+        $moneyCategories[$row["CategoryName"]] += doubleval(str_replace(',', '.', $row["Value"]));
+
+        if (!isset($moneyByDate[$row["EntryDate"]])) { //If the date has no value yet, set it.
+            $moneyByDate[$row["EntryDate"]] = 0;
+        }
+        $moneyByDate[$row["EntryDate"]] += doubleval(str_replace(',', '.', $row["Value"]));
+
+        $monthNum = substr($row["EntryDate"], 5, -3); //Get month number
+        $monthName = date('F', mktime(0, 0, 0, $monthNum, 10));
+        if (!isset($moneyByMonth[$monthName])) { //If the month has no value yet, set it.
+            $moneyByMonth[$monthName] = 0;
+        }
+        $moneyByMonth[$monthName] += doubleval(str_replace(',', '.', $row["Value"]));
+    }
+} else {
+    //echo "You didn't spend money since your last paycheck. ";
+}
+arsort($moneyCategories);
+?>
 
 <script>
     var ctx = document.getElementById("moneySpent");
@@ -193,8 +224,79 @@ echo substr($borderString, 0, -2); //Cut last ,
                         }
                     }]
             },
-            onClick: barChartOpenStatements
+            onClick: chartOpenStatements
 
+        }
+    });</script>
+
+
+
+
+
+
+<script>
+    var ctx = document.getElementById("moneyTime");
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+<?php
+$motLabels = "";
+foreach ($moneyByMonth as $key => $value) {
+    $motLabels .= '"' . $key . '", ';
+}
+echo substr($motLabels, 0, -2); //Cut last ,
+?>
+            ],
+            datasets: [{
+                    label: '',
+                    data: [
+<?php
+$motString = "";
+$motLeft = 0;
+foreach ($moneyByMonth as $key => $value) {
+    $motLeft += floatval($value);
+    $motString .= '"' . $motLeft . '", ';
+}
+echo substr($motString, 0, -2); //Cut last ,
+?>
+                    ],
+                    backgroundColor: [
+                        'rgba(<?php echo $colorMoneyOverTime; ?>, <?php echo $chartFill; ?>)'
+                    ],
+                    borderColor: [
+                        'rgba(<?php echo $colorMoneyOverTime; ?>, <?php echo $chartBorder; ?>)'
+                    ],
+                    borderWidth: <?php echo $chartBorderWidth; ?>
+                }]
+        },
+        options: {
+            plugins: {
+                datalabels: {
+                    align: 'start',
+                    anchor: 'end',
+                    font: {
+                        weight: 'bold'
+                    }
+                }
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            fontSize: <?php echo $chartFontSize; ?>
+                        }
+                    }],
+                xAxes: [{
+                        ticks: {
+                            fontSize: <?php echo $chartFontSize; ?>
+                        }
+                    }]
+            },
+            onClick: chartOpenStatements
         }
     });
 </script>
